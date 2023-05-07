71d0d1c7de5dee6a7bef6ef5db8785b586e7da40
Antonio <antoniorivera.cs18@gmail.com>
2023-05-07 10:43:17 -0400

    Added ids to models for ease of access

--- Models/Project.cs
+++ Models/Project.cs
@@ -3,10 +3,12 @@ namespace TimeTracker.Models
 {
     public class Project
     {
+        public Guid Id { get; init; }
         public decimal Time { get; set; }
         public string Name { get; set; }
         public Project(decimal time, string name)
         {
+            Id = Guid.NewGuid();
             Time = time;
             Name = name;
         }
--- Models/WorkItem.cs
+++ Models/WorkItem.cs
@@ -3,10 +3,12 @@ namespace TimeTracker.Models
 {
     public class WorkItem
     {
+        public Guid Id { get; init; }
         public string Name { get; set; }
         public Project Project { get; set; }
         public WorkItem(string name, Project project)
         {
+            Id = Guid.NewGuid();
             Name = name;
             Project = project;
         }
