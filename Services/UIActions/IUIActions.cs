348b723498d2f18dc4736fa56004b167ff13d054
Antonio <antoniorivera.cs18@gmail.com>
2023-06-11 09:26:25 -0400

    Separated UI modification methods into its own service

---
+++ Services/UIActions/IUIActions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Components;
+
+namespace TimeTracker.Services.UIActions
+{
+    public interface IUIActions
+    {
+        void ExpandTextArea(ChangeEventArgs e, ref string width, ref string height);
+        void SetHeight(ChangeEventArgs e, ref string height);
+        void SetWidth(ChangeEventArgs e, ref string width);
+    }
+}
+
---
+++ Services/UIActions/UIActions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Components;
+
+namespace TimeTracker.Services.UIActions
+{
+    public partial class UIActions : IUIActions
+    {
+
+
+        public void ExpandTextArea(ChangeEventArgs e, ref string width, ref string height)
+        {
+            SetWidth(e, ref width);
+            SetWidth(e, ref height);
+        }
+
+        public void SetHeight(ChangeEventArgs e, ref string height)
+        {
+            if (!string.IsNullOrEmpty((string?)e.Value))
+            {
+                string textValue = (string?)e.Value!;
+                Regex newLines = MyRegex();
+                MatchCollection lineMatches = newLines.Matches(textValue);
+                int matchesCount = lineMatches.Count;
+
+                if (matchesCount > 0)
+                {
+                    double newHeight = 52.4 + matchesCount * 50;
+                    height = newHeight.ToString() + "px";
+                }
+            }
+        }
+
+        public void SetWidth(ChangeEventArgs e, ref string width)
+        {
+            if (!string.IsNullOrEmpty((string?)e.Value))
+            {
+                string text = (string?)e.Value!;
+                int x = text.Length;
+                double newWidth = 35 * Math.Sqrt(x) + 30;
+                width = newWidth.ToString() + "px";
+
+            }
+        }
+
+        [GeneratedRegex("\\n")]
+        private static partial Regex MyRegex();
+    }
+}
+
